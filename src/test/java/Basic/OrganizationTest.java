1d2ac22bded0ec8d592f0e205c8c740e85cdf90e
BhagyashriP94 <bhagyashrithakare01@gmail.com>
2024-03-16 15:05:04 +0530

    Update

---
+++ src/test/java/Basic/OrganizationTest.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+
+	@Test
+	public void CreateOrganizationTest() {
+		 ChromeDriver driver = new ChromeDriver();
+		 driver.get("http://localhost:8888/");
+	}
+}
